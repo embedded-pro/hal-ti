49f29bcb5cc91ac2f840d0788d6bfff7d58f1a94
Gabriel Santos <gabriel.fra.santos@gmail.com>
2026-05-30 17:41:47 +0000

    add  syscall implementation

--- hal_tiva/default_init/DefaultInit.cpp
+++ hal_tiva/default_init/DefaultInit.cpp
@@ -9,6 +9,7 @@
 #error "MCU family not defined or invalid [TM4C123 | TM4C129]!"
 #endif
 #include <errno.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 
 extern "C"
@@ -44,6 +45,41 @@ extern "C"
         __BKPT();
         NVIC_SystemReset();
         __builtin_unreachable();
+    }
+
+    int _close(int /* fd */)
+    {
+        errno = ENOSYS;
+        return -1;
+    }
+
+    int _fstat(int /* fd */, struct stat* st)
+    {
+        st->st_mode = S_IFCHR;
+        return 0;
+    }
+
+    int _isatty(int /* fd */)
+    {
+        return 1;
+    }
+
+    int _lseek(int /* fd */, int /* ptr */, int /* dir */)
+    {
+        errno = ENOSYS;
+        return -1;
+    }
+
+    int _read(int /* fd */, char* /* ptr */, int /* len */)
+    {
+        errno = ENOSYS;
+        return -1;
+    }
+
+    int _write(int /* fd */, char* /* ptr */, int /* len */)
+    {
+        errno = ENOSYS;
+        return -1;
     }
 
     void _init()
